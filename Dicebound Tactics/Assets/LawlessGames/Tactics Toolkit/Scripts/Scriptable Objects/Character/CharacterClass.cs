30869870c28a5928abf58886b7b95878386e6410
ajacobson247 <ajacobson247@gmail.com>
2025-06-13 13:20:53 -0500

    slight addition to classes

--- Dicebound Tactics/Assets/LawlessGames/Tactics Toolkit/Scripts/Scriptable Objects/Character/CharacterClass.cs
+++ Dicebound Tactics/Assets/LawlessGames/Tactics Toolkit/Scripts/Scriptable Objects/Character/CharacterClass.cs
@@ -7,6 +7,11 @@ namespace TacticsToolkit
     [CreateAssetMenu(fileName = "CharacterClass", menuName = "ScriptableObjects/CharacterClass", order = 1)]
     public class CharacterClass : ScriptableObject
     {
+        [Header("Class Info")]
+        public string className;
+        public Sprite classIcon;
+
+        [Header("Base Stats")]
         public BaseStat Health;
         public BaseStat Mana;
         public BaseStat Strenght;
